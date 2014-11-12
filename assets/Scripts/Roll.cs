c4bad7cc384ab7a279c9d80daafe2925daa613b3
Markus Koski <mkoski@Markus-Koskis-iMac.local>
2014-11-12 18:11:59 +0200

    Spaceship steering controls & rolling effect

---
+++ assets/Scripts/Roll.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Roll : MonoBehaviour {
+
+	public float maxTurnRate = 0f;
+	public float turnSensitivity = 0f;
+	private Transform _transform;
+	private Vector3 turnRate = Vector3.zero;
+	private float rotationZ = 0f;
+	private Vector3 center = Vector3.zero;
+	
+	void Start() {
+		_transform = transform; // Cache
+	}
+
+	void Update () {
+		SteerCheck ();
+		_transform.Rotate (turnRate * Time.deltaTime, Space.Self);
+	}
+	
+	void SteerCheck() {
+		if (this == null) { return;}
+		Debug.Log (rotationZ);
+		//rotationZ = Mathf.Lerp (rotationZ, 0f, 0.2f);
+		rotationZ = 0f;
+		rotationZ +=  Input.GetAxis("Horizontal") * 3000 * Time.deltaTime;
+		rotationZ = Mathf.Clamp (rotationZ, -45, 45);
+		_transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, -rotationZ);
+	}
+}
---
+++ assets/Scripts/SpaceshipSteering.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipSteering : MonoBehaviour {
+
+	public float maxTurnRate = 0f;
+	public float turnSensitivity = 0f;
+	private Transform _transform;
+	private Vector3 turnRate = Vector3.zero;
+	private float inputX = 0f;
+	private float inputY = 0f;
+
+	void Start() {
+		_transform = transform; // Cache
+	}
+
+	void LateUpdate () {
+		SteerCheck ();
+		_transform.Rotate (turnRate * Time.deltaTime, Space.Self); // Rotate spaceship
+	} 
+
+	void SteerCheck() {
+		if (this == null) { return;}
+		inputX = 0f;
+		inputY = 0f;
+		inputX +=  Input.GetAxis("Vertical") * turnSensitivity * Time.deltaTime;	
+		inputY +=  Input.GetAxis("Horizontal") * turnSensitivity * Time.deltaTime;
+		SetTurnRate (inputX, inputY, 0);	
+	}
+	
+	private void SetTurnRate (float x, float y, float z) {
+		turnRate.x = Mathf.Clamp (x, -maxTurnRate, maxTurnRate);
+		turnRate.y = Mathf.Clamp (y, -maxTurnRate, maxTurnRate);
+		turnRate.z = Mathf.Clamp (z, -maxTurnRate, maxTurnRate);
+	}
+}
